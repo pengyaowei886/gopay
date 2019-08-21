47a4174566a29415431ca88a64b2bd414ad0d7e7
pengyaowei <pengyaowei886@163.com>
2019-08-21 09:35:16 +0800

    ds

--- config/config.prod.js
+++ config/config.prod.js
@@ -48,7 +48,7 @@ module.exports = appInfo => {
   config.cluster = {
     listen: {
       port: 7001,
-      hostname: '47.103.63.239',
+      hostname: '172.19.87.165',
       // https:{
       //   key:"D:/work/ssl/0_xingyumeng.com.key",
       //   cert:"D:/worssl/1_xingyumeng.com_bundle.pem"
