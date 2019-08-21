ca6e509e27ce15650fab76be3551d7c6731c9b20
pengyaowei <pengyaowei886@163.com>
2019-08-21 09:11:28 +0800

    ds

--- config/config.prod.js
+++ config/config.prod.js
@@ -24,7 +24,7 @@ module.exports = appInfo => {
     // 单数据库信息配置
     client: {
       // host
-      host: '172.19.87.165',
+      host: '127.0.0.1',
       // 端口号
       port: '3306',
       // 用户名
