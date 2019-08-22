68d94608627095c184594a62130390f21e9299ac
pengyaowei <pengyaowei886@163.com>
2019-08-22 15:27:22 +0800

    sa

--- app/io/middleware/conn.js
+++ app/io/middleware/conn.js
@@ -1,9 +1,11 @@
 module.exports = app => {
     return async (ctx, next) => {
         //验证签名
+        ctx.logger.info("有人进来了");
+
         ctx.socket.emit('res', 'connected!');
 
-        
+
         await next();
         // execute when disconnect.
 
